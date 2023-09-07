959d8a054d8fc91c4e6bb1bb4296996803d3e7b8
Jean-Baptiste BOUGAUD <jbaptiste.bougaud@gmail.com>
2023-09-07 13:55:12 +0200

    Add User Class

---
+++ src/main/java/jpa/Client.java
@@ -0,0 +1,8 @@
+package jpa;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class Client {
+
+}
---
+++ src/main/java/jpa/User.java
@@ -0,0 +1,50 @@
+package jpa;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public abstract class User {
+    private Long id;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    public User(String username, String password) {
+        this.email = username;
+        this.password = password;
+    }
+
+    public User() {
+
+    }
+
+    @Id
+    @GeneratedValue
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
